712099929dd3572e0918cfe13ea97f743e2850a3
CarlosMatallana-prog <3496@holbertonschool.com>
2022-02-15 20:32:03 -0500

    :hammer: function that sorts an array of integers

---
+++ 0-bubble_sort.c
@@ -0,0 +1,29 @@
+#include "sort.h"
+
+/**
+ * bubble_sort - Sorts an array of integers
+ * @array:      -     Array of integers.
+ * @size:      -     Size of the given array.
+ *
+ * Return: void
+ */
+void bubble_sort(int *array, size_t size)
+{
+	unsigned int i, swapped = 1;
+	int tmp;
+
+	while (swapped)
+	{
+		swapped = 0;
+		for (i = 1; i < size; i++)
+
+			if (array[i - 1] > array[i])
+			{
+				tmp = array[i - 1];
+				array[i - 1] = array[i];
+				array[i] = tmp;
+				print_array(array, size);
+				swapped = 1;
+			}
+	}
+}
